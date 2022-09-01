80d3ffb9ccaeeca906634646c54d1a781e9ac56a
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-09-01 15:46:13 +0200

    Testing.

--- src/index.js
+++ src/index.js
@@ -12,16 +12,17 @@ async function run() {
     // Iterate over the instances.
     for (const reservation of data.Reservations) {
       for (const instance of reservation.Instances) {
-        const NAME_TAG = `${REPO_NAME} Github Runner`;
-        if (instance.State.Name === 'running') {
-          for (const tag of instance.Tags) {
-            // Check if there are instances that need to be removed.
-            if (tag.Key === 'Name' && tag.Value === NAME_TAG) {
-              const ID = instance.InstanceId;
-              params.InstanceIds.push(ID);
-            }
-          }
-        }
+        console.log(instance)
+        // const NAME_TAG = `${REPO_NAME} Github Runner`;
+        // if (instance.State.Name === 'running') {
+        //   for (const tag of instance.Tags) {
+        //     // Check if there are instances that need to be removed.
+        //     if (tag.Key === 'Name' && tag.Value === NAME_TAG) {
+        //       const ID = instance.InstanceId;
+        //       params.InstanceIds.push(ID);
+        //     }
+        //   }
+        // }
       }
     }
 
