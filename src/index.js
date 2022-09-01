cbad98739eeb09e4628ff0c8a106ff6f892a4cad
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-09-01 12:57:06 +0200

    Add running instance filter.

--- src/index.js
+++ src/index.js
@@ -12,13 +12,14 @@ async function run() {
     // Iterate over the instances.
     for (const reservation of data.Reservations) {
       for (const instance of reservation.Instances) {
-        console.log(instance);
         const NAME_TAG = `${REPO_NAME} Github Runner`;
-        for (const tag of instance.Tags) {
-          // Check if there are instances that need to be removed.
-          if (tag.Key === 'Name' && tag.Value === NAME_TAG) {
-            const ID = instance.InstanceId;
-            params.InstanceIds.push(ID);
+        if (instance.State.Name === 'running') {
+          for (const tag of instance.Tags) {
+            // Check if there are instances that need to be removed.
+            if (tag.Key === 'Name' && tag.Value === NAME_TAG) {
+              const ID = instance.InstanceId;
+              params.InstanceIds.push(ID);
+            }
           }
         }
       }
