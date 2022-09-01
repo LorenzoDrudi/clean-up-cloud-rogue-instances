8a90302723546faad6c561595e464298c257fe40
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-09-01 12:34:58 +0200

    Refactoring.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 const core = require('@actions/core');
-const { DescribeInstancesCommand, TerminateInstancesCommand } = require('@aws-sdk/client-ec2');
 const { ec2Client } = require('./ec2Client');
+const { DescribeInstancesCommand, TerminateInstancesCommand } = require('@aws-sdk/client-ec2');
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
     const REPO_NAME = core.getInput('repo-name');
     const params = { InstanceIds: []};
-
     const data = await ec2Client.send(new DescribeInstancesCommand({}));
+
     for (const reservation of data.Reservations) {
       for (const instance of reservation.Instances) {
-        const NAME_TAG = REPO_NAME + " Github Runner";
-        console.log(NAME_TAG);
+        const NAME_TAG = `${REPO_NAME} Github Runner`;
         for (const tag of instance.Tags) {
-          console.log(tag.Value);
           if (tag.Key === 'Name' && tag.Value === NAME_TAG) {
             const ID = instance.InstanceId;
-            console.log(ID);
             params.InstanceIds.push(ID);
           }
         }
       }
     }
-    console.log(params.InstanceIds);
-    await ec2Client.send(new TerminateInstancesCommand(params));
+
+    if (params.InstanceIds.length) {
+      await ec2Client.send(new TerminateInstancesCommand(params));
+      core.notice(`Removed Rogue Instances ${params.InstanceIds.toString()}`);
+    } else {
+      core.notice(`No Rogue Instances to remove`);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
