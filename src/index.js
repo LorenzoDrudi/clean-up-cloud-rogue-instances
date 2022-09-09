232d6ca7089eddea49aabe3374c0bdc8def8453d
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-09-09 18:08:24 +0200

    v1.1.1

--- src/index.js
+++ src/index.js
@@ -7,27 +7,26 @@ async function run() {
   try {
     const REPO_NAME = core.getInput('repo-name');
     const REPO_OWNER = core.getInput('repo-owner');
+    const NAME_TAG = `${REPO_NAME} Github Runner`;
     const ROGUE_INSTANCE = 2;
-    const PARAMS = { InstanceIds: []};
+    const PARAMS = { InstanceIds: [] };
     // Get all the infos about the instances in the specified aws region.
     const DATA = await ec2Client.send(new DescribeInstancesCommand({}));
     // Get all the runners linked to the repo
     const RUNNERS_NAME = await getRepoRunningRunnersName(REPO_NAME, REPO_OWNER);
 
     // Iterate over the instances.
-    for (const reservation of DATA.Reservations) {
-      for (const instance of reservation.Instances) {
-        if (instance.State.Name === 'running') {
-          const NAME_TAG = `${REPO_NAME} Github Runner`;
-          const CHECK = instance.Tags
-            .filter(tag => (tag.Key === 'Name' && tag.Value === NAME_TAG)
-              || (tag.Key === 'Runner' && !RUNNERS_NAME.includes(tag.Value)))
-            .length;
-          if (CHECK === ROGUE_INSTANCE) {
-            const ID = instance.InstanceId;
-            PARAMS.InstanceIds.push(ID);
-          }
-        }
+    const RUNNING_INSTANCES = DATA.Reservations
+      .flatMap(reservation => reservation.Instances)
+      .filter(instance => instance.State.Name === 'running');
+    for (const instance of RUNNING_INSTANCES) {
+      const CHECK = instance.Tags
+        .filter(tag => (tag.Key === 'Name' && tag.Value === NAME_TAG)
+          || (tag.Key === 'Runner' && !RUNNERS_NAME.includes(tag.Value)))
+        .length;
+      if (CHECK === ROGUE_INSTANCE) {
+        const ID = instance.InstanceId;
+        PARAMS.InstanceIds.push(ID);
       }
     }
 
